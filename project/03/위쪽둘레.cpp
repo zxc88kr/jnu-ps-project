43f0d17b9371e9997bbb9b99db00e95a1f7c749c
zxc88kr <zxc88kr@naver.com>
2023-04-27 12:38:19 +0900

    Project 03 Description 2

--- project/03/위쪽둘레.cpp
+++ project/03/위쪽둘레.cpp
@@ -2,17 +2,25 @@
 // 학번 : 195443
 // 이름 : 박찬
 
-#include <cstdio>
-#include <vector>
-#include <cmath>
-#include <algorithm>
+#include <cstdio> // 표준 입출력 라이브러리
+#include <vector> // 벡터 라이브러리
+#include <cmath> // 수학 라이브러리
+#include <algorithm> // 알고리즘 라이브러리
+#include <ctime> // 시간 라이브러리
 
+/**
+ * 중복을 포함한 세 카드의 합으로 만들 수 있는 당첨번호 벡터를 반환하는 함수
+ * @param n			  카드의 수
+ * @param shortLength 카드 벡터 shortLength[0] ~ shortLength[n - 1]
+ * @param longLength  당첨번호 벡터 longLength[0] ~ longLength[n - 1]
+ * @return			  중복을 포함한 세 카드의 합으로 만들 수 있는 당첨번호 벡터를 반환
+ */
 int solve(int n, std::vector<int>& shortLength, std::vector<int>& longLength)
 {
-	std::vector<int> lastStandPath(n);
-	std::vector<int> lastLiePath(n);
-	lastStandPath[0] = shortLength[0];
-	lastLiePath[0] = longLength[0];
+	std::vector<int> lastStandRound(n);
+	std::vector<int> lastLieRound(n);
+	lastStandRound[0] = shortLength[0];
+	lastLieRound[0] = longLength[0];
 
 	for (int i = 1; i < n; i++)
 	{
@@ -21,12 +29,12 @@ int solve(int n, std::vector<int>& shortLength, std::vector<int>& longLength)
 		int currentShort = shortLength[i];
 		int currentLong = longLength[i];
 
-		lastStandPath[i] = std::max(lastStandPath[i - 1] + std::abs(currentLong - previousLong) + currentShort, // 서있고 서있다
-									lastLiePath[i - 1] + std::abs(currentLong - previousShort) + currentShort); // 누워있고 서있다
-		lastLiePath[i] = std::max(lastStandPath[i - 1] + std::abs(currentShort - previousLong) + currentLong, // 서있고 누워있다
-								  lastLiePath[i - 1] + std::abs(currentShort - previousShort) + currentLong); // 누워있고 누워있다
+		lastStandRound[i] = std::max(lastStandRound[i - 1] + std::abs(currentLong - previousLong) + currentShort, // 서있고 서있다
+									lastLieRound[i - 1] + std::abs(currentLong - previousShort) + currentShort); // 누워있고 서있다
+		lastLieRound[i] = std::max(lastStandRound[i - 1] + std::abs(currentShort - previousLong) + currentLong, // 서있고 누워있다
+								  lastLieRound[i - 1] + std::abs(currentShort - previousShort) + currentLong); // 누워있고 누워있다
 	}
-	return std::max(lastStandPath[n - 1], lastLiePath[n - 1]);
+	return std::max(lastStandRound[n - 1], lastLieRound[n - 1]);
 }
 
 int main()
@@ -39,9 +47,14 @@ int main()
 	for (int i = 0; i < n; i++)
 		scanf("%d %d", &shortLength[i], &longLength[i]);
 	
-	int path = solve(n, shortLength, longLength);
+	clock_t start = clock(); // 시간 측정 시작
+	
+	int round = solve(n, shortLength, longLength);
+	printf("%d\n", round);
 
-	printf("%d\n", path);
+	clock_t end = clock(); // 시간 측정 종료
+
+	printf("실행시간: %lf초\n", (double)(end - start) / CLOCKS_PER_SEC);
 
 	return 0;
 }
--- project/03/카드게임.cpp
+++ project/03/카드게임.cpp
@@ -2,30 +2,39 @@
 // 학번 : 195443
 // 이름 : 박찬
 
-#include <cstdio>
-#include <vector>
+#include <cstdio> // 표준 입출력 라이브러리
+#include <vector> // 벡터 라이브러리
+#include <ctime> // 시간 라이브러리
 
-void solve(int a, int b, std::vector<int>& cardA, std::vector<int>& cardB)
+/**
+ * 주어진 두 카드 더미로 게임을 했을 때 얻을 수 있는 최대 점수를 반환하는 함수
+ * @param a		첫 번째 카드 더미의 카드 수
+ * @param b		두 번째 카드 더미의 카드 수
+ * @param cardA 첫 번째 카드 더미 벡터 cardA[0] ~ cardA[a - 1]
+ * @param cardB 두 번째 카드 더미 벡터 cardB[0] ~ cardB[b - 1]
+ * @return		주어진 두 카드 더미로 게임을 했을 때 얻을 수 있는 최대 점수를 반환
+ */
+int getMaxScore(int a, int b, std::vector<int>& cardA, std::vector<int>& cardB)
 {
-	int maxScore = 0;
-	for (int beginB = 0; beginB < b; beginB++)
+	int maxScore = 0; // 얻을 수 있는 최대 점수를 0으로 초기화
+	for (int beginB = 0; beginB < b; beginB++) // 두 번째 카드 더미를 기준으로 비교 진행, 카드 더미 위에서 버릴 카드 수를 구함
 	{
-		int idxA = 0;
-		int idxB = beginB;
-		int match = 0;
+		int idxA = 0; // 첫 번째 카드 더미는 첫 카드부터 비교 시작
+		int idxB = beginB; // 두 번째 카드 더미는 위에서 버린 카드들 다음 카드부터 비교 시작
+		int match = 0; // 비교 결과 두 카드가 동일
 
-		while (idxA < a && idxB < b)
+		while (idxA < a && idxB < b) // 두 카드 더미 중 어느 하나라도 탐색이 끝나면 비교 종료
 		{
-			if (cardA[idxA] == cardB[idxB])
+			if (cardA[idxA] == cardB[idxB]) // 두 카드의 숫자가 동일한 경우
 			{
-				idxB++;
-				match++;
+				idxB++; // 두 번째 카드 더미의 다음 카드를 탐색
+				match++; // 일치 결과 1 증가
 			}
-			idxA++;
+			idxA++; // 첫 번째 카드 더미의 다음 카드를 탐색
 		}
-		maxScore = ((match > maxScore) ? match : maxScore);
+		maxScore = ((match > maxScore) ? match : maxScore); // 최대 점수 업데이트
 	}
-	printf("%d\n", maxScore);
+	return maxScore; // 최대 점수 반환
 }
 
 int main()
@@ -41,7 +50,14 @@ int main()
 	for (int i = 0; i < b; i++)
 		scanf("%d", &cardB[i]);
 
-	solve(a, b, cardA, cardB);
+	clock_t start = clock(); // 시간 측정 시작
+	
+	int maxScore = getMaxScore(a, b, cardA, cardB);
+	printf("%d\n", maxScore);
+
+	clock_t end = clock(); // 시간 측정 종료
+
+	printf("실행시간: %lf초\n", (double)(end - start) / CLOCKS_PER_SEC);
 
 	return 0;
 }
