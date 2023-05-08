d7670169d708dc1c017d7b4bea978f92718f034a
zxc88kr <zxc88kr@naver.com>
2023-05-08 13:12:58 +0900

    10 Weeks Description 1

---
+++ practice/08/불안정 지역.cpp
@@ -0,0 +1,89 @@
+// 문제해결프로젝트 실습 08 (2)
+// 학번 : 195443
+// 이름 : 박찬
+
+#include <cstdio>
+#include <vector>
+#include <queue>
+
+struct City
+{
+    int index; // 도시의 인덱스
+    int income; // 도시의 소득 수준
+
+    // 생성자
+    City() : index(-1), income(-1) {}
+    City(int idx, int incm) : index(idx), income(incm) {}
+
+    // 연산자 오버로딩 (소득 수준을 기준으로 비교)
+    bool operator<(const City& city) const
+    {
+        return this->income < city.income;
+    }
+    bool operator>(const City& city) const
+    {
+        return this->income > city.income;
+    }
+};
+
+/**
+ * 연속한 k개의 도시로 이루어진 영역들 중 가장 큰 소득차를 반환하는 함수
+ * @param cities 도시의 구조체 벡터 cities[0] ~ cities[n - 1]
+ * @param n      도시의 수
+ * @param k      연속한 도시의 수
+ * @return       연속한 k개의 도시로 이루어진 영역들 중 가장 큰 소득차를 가지는 영역의 소득차를 반환
+ */
+int getMaxDifferenceInRange(std::vector<City>& cities, int n, int k)
+{
+    int maxDifference = 0; // 가장 큰 소득차
+
+    // 소득이 가장 높은 도시부터 pop되는 우선순위 큐
+    std::priority_queue<City, std::vector<City>, std::less<City>> maximumInRange;
+    // 소득이 가장 낮은 도시부터 pop되는 우선순위 큐
+    std::priority_queue<City, std::vector<City>, std::greater<City>> minimumInRange;
+
+    for (int i = 0; i < k - 1; i++) // k - 1 개의 도시 정보를 우선순위 큐에 push
+    {
+        maximumInRange.push(cities[i]);
+        minimumInRange.push(cities[i]);
+    }
+    for (int i = k - 1; i < n; i++) // 나머지 도시 정보를 정해진 규칙에 따라 우선순위 큐에 push
+    {
+        maximumInRange.push(cities[i]);
+        minimumInRange.push(cities[i]);
+
+        // 최대, 최소 소득을 가진 도시가 검사 범위 밖에 있다면, 이에 해당하는 도시들을 전부 우선순위 큐에서 pop
+        while (maximumInRange.top().index < i - (k - 1)) maximumInRange.pop();
+        while (minimumInRange.top().index < i - (k - 1)) minimumInRange.pop();
+
+        // 현재 영억에서의 소득차를 기준으로 가장 큰 소득차 값을 업데이트
+        int difference = maximumInRange.top().income - minimumInRange.top().income;
+        maxDifference = ((difference > maxDifference) ? difference : maxDifference);
+    }
+    return maxDifference;
+}
+
+int main()
+{
+	int t;
+	scanf("%d", &t);
+
+	for (int idx = 0; idx < t; idx++)
+	{
+		int n, k;
+		scanf("%d %d", &n, &k);
+
+        std::vector<City> cities(n);
+        for (int i = 0; i < n; i++)
+        {
+            // 입력받은 소득 수준을 바탕으로 0번째 도시의 구조체부터 생성
+            int income;
+            scanf("%d", &income);
+            cities[i] = City(i, income);
+        }
+        int maxDifference = getMaxDifferenceInRange(cities, n, k);
+        printf("%d\n", maxDifference);
+	}
+
+    return 0;
+}
---
+++ practice/08/조세퍼스의 문제.cpp
@@ -0,0 +1,65 @@
+// 문제해결프로젝트 실습 08 (1)
+// 학번 : 195443
+// 이름 : 박찬
+
+#include <cstdio>
+#include <vector>
+#include <queue>
+
+/**
+ * 각 번호의 사람들이 정해진 규칙대로 게임을 진행했을 때 게임에서 제외되는 사람 순서 벡터를 반환하는 함수
+ * @param n 게임에 참여하는 사람의 수
+ * @param m 게임 규칙 (m 번째 사람을 반복적으로 제외)
+ * @return  각 번호의 사람들이 정해진 규칙대로 게임을 진행했을 때 게임에서 제외되는 사람 순서 벡터를 반환
+ */
+std::vector<int> getDeadPlayers(int n, int m)
+{
+    std::vector<int> deadPlayers; // 게임에서 제외되는 사람 순서 벡터
+
+    std::queue<int> players; // 아직 게임에서 제외되지 않은 사람 큐
+    for (int i = 0; i < n; i++) // 1 ~ n 번째 사람이 게임에 참여
+        players.push(i + 1);
+
+    for (int i = 0; i < n; i++) // 모든 사람이 게임에서 제외될 때까지 게임을 진행
+    {
+        int skip = m - 1; // m - 1 명의 사람을 건너뛰고 그 다음 사람을 게임에서 제외시킴
+
+        // 건너뛴 사람은 우선 큐에서 pop 한 다음, 큐의 마지막 대기열로 다시 push
+        while (skip > 0)
+        {
+            int skipPlayer = players.front();
+            players.pop();
+            players.push(skipPlayer);
+            skip--;
+        }
+
+        // 제외되는 사람은 우선 큐에서 pop 한 다음, 제외되는 사람 순서 벡터에 push
+        int deadPlayer = players.front();
+        players.pop();
+        deadPlayers.push_back(deadPlayer);
+    }
+    return deadPlayers;
+}
+
+int main()
+{
+	int t;
+	scanf("%d", &t);
+
+	for (int idx = 0; idx < t; idx++)
+	{
+		int n, m;
+		scanf("%d %d", &n, &m);
+
+        std::vector<int> deadPlayers = getDeadPlayers(n, m);
+
+        for (int i = 0; i < deadPlayers.size(); i++)
+        {
+            if (i > 0) printf(" "); // 각 데이터는 공백으로 구분
+            printf("%d", deadPlayers[i]);
+        }
+        printf("\n");
+	}
+
+    return 0;
+}
