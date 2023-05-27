da0d69f993e90c6527184010944d31bd384d2c60
zxc88kr <zxc88kr@naver.com>
2023-05-27 18:20:54 +0900

    13 Weeks Description 2

--- practice/11/깊이우선탐색.cpp
+++ practice/11/깊이우선탐색.cpp
@@ -4,16 +4,21 @@
 
 #include <cstdio>
 
-#define MAX_DEPTH 8
+#define MAX_DEPTH 8 // 그래프의 최대 깊이
 
-bool connected[MAX_DEPTH + 1][MAX_DEPTH + 1] = { false, };
-bool visited[MAX_DEPTH + 1] = { false, };
+bool connected[MAX_DEPTH + 1][MAX_DEPTH + 1] = { false, }; // 인접 행렬을 false로 초기화
+bool visited[MAX_DEPTH + 1] = { false, }; // 방문 여부 배열을 false로 초기화
 
+/**
+ * 깊이우선탐색 방법으로 그래프를 순회하며 탐색한 노드들을 순서대로 하나씩 출력하는 함수
+ * @param v 현재 탐색 중인 노드의 번호
+ */
 void dfs(int v)
 {
-	printf("%d\n", v);
-	visited[v] = true;
+	printf("%d\n", v); // 현재 노드를 출력
+	visited[v] = true; // 현재 노드의 방문 여부를 true로 체크
 
+	// 연결된 노드들 중에서 아직 방문하지 않은 노드들을 재귀적으로 탐색
 	for (int i = 1; i <= MAX_DEPTH; i++)
 		if (connected[v][i] && !visited[i])
 			dfs(i);
@@ -21,17 +26,18 @@ void dfs(int v)
 
 int main()
 {
-	int n;
-	scanf("%d", &n);
+	int edge_num;
+	scanf("%d", &edge_num);
 
-	for (int i = 0; i < n; i++)
+	for (int i = 0; i < edge_num; i++)
 	{
+		// 그래프 노드 간의 연결 상태를 입력 받은 뒤 인접 행렬에 반영
 		int v1, v2;
 		scanf("%d %d", &v1, &v2);
 		connected[v1][v2] = connected[v2][v1] = true;
 	}
 	
-	dfs(1);
+	dfs(1); // 1번 노드부터 탐색 시작
 	
 	return 0;
 }
--- practice/11/너비우선탐색.cpp
+++ practice/11/너비우선탐색.cpp
@@ -5,31 +5,37 @@
 #include <cstdio>
 #include <queue>
 
-#define MAX_DEPTH 8
+#define MAX_DEPTH 8 // 그래프의 최대 깊이
 
-bool connected[MAX_DEPTH + 1][MAX_DEPTH + 1] = { false, };
-bool visited[MAX_DEPTH + 1] = { false, };
+bool connected[MAX_DEPTH + 1][MAX_DEPTH + 1] = { false, }; // 인접 행렬을 false로 초기화
+bool visited[MAX_DEPTH + 1] = { false, }; // 방문 여부 배열을 false로 초기화
 
-std::queue<int> q;
-int num, edge_num;
+std::queue<int> q; // 방문할 노드를 예약하는 큐
+int num, edge_num; // 노드의 수, 엣지의 수
 
+/**
+ * 너비우선탐색 방법으로 그래프를 순회하며 탐색한 노드들을 순서대로 하나씩 출력하는 함수
+ * @param v 현재 탐색 중인 노드의 번호
+ */
 void bfs(int v)
 {
-	printf("%d\n", v);
-	q.push(v);
-	visited[v] = true;
+	printf("%d\n", v); // 현재 노드를 출력
+	q.push(v); // 현재 노드를 큐에 push
+	visited[v] = true; // 현재 노드의 방문 여부를 true로 체크
 
-	while (!q.empty())
+	while (!q.empty()) // 아직 방문하지 않은 노드가 큐에 남아 있는 경우
 	{
+		// 큐에서 가장 앞에 있는 노드를 기준이 되는 노드로 설정하고 큐에서 pop
 		v = q.front();
 		q.pop();
 
+		// 연결된 노드들 중에서 아직 방문하지 않은 노드들을 일괄적으로 탐색
 		for (int i = 1; i <= num; i++)
 			if (connected[v][i] && !visited[i])
 			{
-				printf("%d\n", i);
-				q.push(i);
-				visited[i] = true;
+				printf("%d\n", i); // 탐색 노드를 출력
+				q.push(i); // 탐색 노드를 큐에 push
+				visited[i] = true; // 탐색 노드의 방문 여부를 true로 체크
 			}
 	}
 }
@@ -40,12 +46,13 @@ int main()
 
 	for (int i = 0; i < edge_num; i++)
 	{
+		// 그래프 노드 간의 연결 상태를 입력 받은 뒤 인접 행렬에 반영
 		int v1, v2;
 		scanf("%d %d", &v1, &v2);
 		connected[v1][v2] = connected[v2][v1] = true;
 	}
 
-	bfs(1);
+	bfs(1); // 1번 노드부터 탐색 시작
 
 	return 0;
 }
